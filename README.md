f777f34e54d16eee84d3e225ba5d4d9b2d668dab
Alpha Olomi <alphaolomi@gmail.com>
2019-12-29 21:41:19 +0300

    style: lint with prettier

--- README.md
+++ README.md
@@ -7,7 +7,6 @@
 
 Playful and Colorful One-Page portfolio featuring Parallax effects and animations. Using the Gatsby Theme [`@lekoarts/gatsby-theme-cara`](https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-cara).
 
-
 ## ✨ Features
 
 - Theme UI-based theming
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+  path: `.env`
+});
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Gladson Lyimo - Personal Website`,
+    siteTitleAlt: `Gladson Lyimo - Personal Website`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -31,17 +31,17 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-netlify`
+  ]
+};
